f001e892c7804617253ab94589d2d34c9a9778dd
Mikhail Netkachev <mikhailnetkachev@MacBook-Pro-Mikhail.local>
2021-01-17 17:11:40 +0200

    Second step: initialized basic server.

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-console.log("Hello, world!");
+const http = require("http");
+const PORT = 3000;
+
+const server = http.createServer((req, res) => {
+  try {
+    console.log(`URL "${req.url}" has been requested.`);
+    res.end("Wait ...");
+  } catch (error) {
+    res.end("Young man, we have a technical break.");
+  }
+});
+
+server.listen(PORT, () => {
+  console.log(`Server has been started on port ${PORT}.`);
+});
