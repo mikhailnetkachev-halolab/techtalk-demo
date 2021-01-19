f58992671e1d2122a58b012389385500ff3a2818
Mikhail Netkachev <mikhailnetkachev@MacBook-Pro-Mikhail.local>
2021-01-19 23:13:59 +0200

    Third step: created basic API routes.

--- index.js
+++ index.js
@@ -1,10 +1,32 @@
+const fs = require("fs");
 const http = require("http");
+const path = require("path");
 const PORT = 3000;
 
 const server = http.createServer((req, res) => {
   try {
-    console.log(`URL "${req.url}" has been requested.`);
-    res.end("Wait ...");
+    if (req.method === "GET") {
+      fs.readFile(path.resolve(__dirname, "views", "index.html"), (error, data) => {
+        if (error) throw error;
+
+        res.writeHead(200, { "Content-Type": "text/html" });
+        res.end(data);
+      });
+    }
+    
+    if (req.method === "POST") {
+      const body = [];
+
+      req.on("data", (data) => {
+        body.push(Buffer.from(data));
+      });
+      req.on("end", () => {
+        console.log(body);
+
+        res.writeHead(200, { "Content-Type": "application/json" });
+        res.end(JSON.stringify(body.toString()));
+      });
+    }
   } catch (error) {
     res.end("Young man, we have a technical break.");
   }
